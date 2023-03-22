bfacdd46a52ba7c199eeed6b98fcae4f9b344192
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-22 23:00:31 +0530

    adding functions

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
 /**  creating a data set to store the questions  */
-
+let readlineSync = require("readline-sync");
+let score=0;
 const database = {
 
 
-    data: [
-      {
-        question: " Let a={}, b={} then if we do console.log(a==b)and console.log(a===b)",
-        options: {
-          a: "false false",
-          b: "false true",
-          c: "true false",
-          d: "true true"
-        },
-        correctAnswer: "a"
+  data: [
+    {
+      question: " Let a={}, b={} then if we do console.log(a==b)and console.log(a===b)",
+      options: {
+        a: "false false",
+        b: "false true",
+        c: "true false",
+        d: "true true"
       },
-      {
-        question: "Object.assign(target, source) create which type of copy?",
-        options: {
-          a: "Deep copy",
-          b: "Shallow copy",
-          c: "Nested Copy",
-          d: "Creates a new reference"
-        },
-        correctAnswer: "b"
+      correctAnswer: "a"
+    },
+    {
+      question: "Object.assign(target, source) create which type of copy?",
+      options: {
+        a: "Deep copy",
+        b: "Shallow copy",
+        c: "Nested Copy",
+        d: "Creates a new reference"
       },
-      {
-        question: "Is Method chaining possible with forEach?",
-        options: {
-          a: "Yes",
-          b: "No"
-        },
-        correctAnswer: "b"
-      }
-    ]
-  }
-  
-  function showQuestionAndOptions() {
-    for (let i = 0; i < database.data.length; i++) {
-      console.log("Q"[i], database.data[i].question);
+      correctAnswer: "b"
+    },
+    {
+      question: "Is Method chaining possible with forEach?",
+      options: {
+        a: "Yes",
+        b: "No"
+      },
+      correctAnswer: "b"
     }
+  ]
+}
+//function to check the correct and the incorrect answers
+function playGame(userAnswer,correctAnswer){
+  if(userAnswer=== correctAnswer){
+    console.log("Correct Answer");
+    score++;
+  }
+  else{
+    console.log("Incorrect Answer");
+    console.log(`correct answer is ${correctAnswer}`);
   }
   
-  showQuestionAndOptions();
+}
+// function to display the questions and the options
+function showQuestionAndOptions() {
+  for (let i = 0; i < database.data.length; i++) {
+    console.log(`\nQ${i+1}-${database.data[i].question}\n`);
+    for(let key in database.data[i].options){
+      console.log(`${key} - ${database.data[i].options[key]}`);
+    }
+    // taking the input from the user
+    let userAnswer = readlineSync.question("Enter your Answer -(a/b/c/d) - ");
+  }
+  playGame(userAnswer, database.data[i].correctAnswer)
+}
+
+showQuestionAndOptions(database);
+
+console.log(score);
