079971ebee5124a15d21b214f520556627433832
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-25 10:40:45 +0530

    adding functions, to calculate the score and displaying it on leader board

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-/**  creating a data set to store the questions  */
 let readlineSync = require("readline-sync");
-let score=0;
+let kuler = require("kuler");
+let score = 0;
+
+let userName = readlineSync.question("What's your name? ");
+console.log(kuler(`\nHello ${userName} welcome to Quizify`, "#dc2626"))
+
+console.log("\nPlease select any options by typing either a/b/c/d\n")
+
+/** Creating data set */
 const database = {
-
-
   data: [
     {
-      question: " Let a={}, b={} then if we do console.log(a==b)and console.log(a===b)",
+      question: `let a = {}, b = {}
+console.log(a == b)
+console.log(a === b)`,
       options: {
         a: "false false",
         b: "false true",
@@ -16,17 +23,17 @@ const database = {
       correctAnswer: "a"
     },
     {
-      question: "Object.assign(target, source) create which type of copy?",
+      question: "Object.assign(targer, source) creates which type of copy?",
       options: {
-        a: "Deep copy",
-        b: "Shallow copy",
+        a: "Deep Copy",
+        b: "Shallow Copy",
         c: "Nested Copy",
         d: "Creates a new reference"
       },
       correctAnswer: "b"
     },
     {
-      question: "Is Method chaining possible with forEach?",
+      question: "Is method chaining possible with forEach?",
       options: {
         a: "Yes",
         b: "No"
@@ -35,31 +42,57 @@ const database = {
     }
   ]
 }
-//function to check the correct and the incorrect answers
-function playGame(userAnswer,correctAnswer){
-  if(userAnswer=== correctAnswer){
-    console.log("Correct Answer");
-    score++;
-  }
-  else{
-    console.log("Incorrect Answer");
-    console.log(`correct answer is ${correctAnswer}`);
-  }
-  
-}
-// function to display the questions and the options
-function showQuestionAndOptions() {
-  for (let i = 0; i < database.data.length; i++) {
-    console.log(`\nQ${i+1}-${database.data[i].question}\n`);
-    for(let key in database.data[i].options){
-      console.log(`${key} - ${database.data[i].options[key]}`);
+
+
+/** Creating score Board*/
+const leaderBoard = {
+  data: [
+    {
+      name: "Ashish",
+      score: 1
+    },
+    {
+      name: "Riya",
+      score: 3
+    },
+    {
+      name: "Jay",
+      score: 2
     }
-    // taking the input from the user
-    let userAnswer = readlineSync.question("Enter your Answer -(a/b/c/d) - ");
+  ]
+}
+
+/** Main Logic */
+function playGame(userAnswer, correctAnswer) {
+  if (userAnswer === correctAnswer) {
+    console.log(kuler("Correct Answer", "#059669"));
+    score++
+  } else {
+    console.log(kuler("Incorrect Answer", "#b91c1c"));
+    console.log(kuler(`Correct Answer is ${correctAnswer}`, "#1d4ed8"))
   }
-  playGame(userAnswer, database.data[i].correctAnswer)
+}
+//function to show question and options
+function showQuestionAndOptions(database) {
+  for (let i = 0; i < database.data.length; i++) {
+    console.log(`\nQ${i + 1} - ${database.data[i].question}\n`);
+    for (let key in database.data[i].options) {
+      console.log(`${key} - ${database.data[i].options[key]}`)
+    }
+    let userAnswer = readlineSync.question("Enter your answer - (a/b/c/d) - ").toLowerCase();
+    playGame(userAnswer, database.data[i].correctAnswer)
+  }
+}
+//updating the leader board
+function showHighScorer(leaderBoard) {
+  leaderBoard.data.push({ name: userName, score: score })
+  let sortedScoreList = leaderBoard.data.sort((a, b) => b.score - a.score);
+  console.log(kuler("\nCheck your position on the Leader Board🎉🎉", "#fde047"))
+  for (let leader of sortedScoreList) {
+    console.log(kuler(`${leader.name} -  Score: ${leader.score}`, "#9333ea"))
+  }
 }
 
 showQuestionAndOptions(database);
-
-console.log(score);
+console.log(kuler(`\nYour score is - ${score}`, "#5eead4"));
+showHighScorer(leaderBoard);
