a3a5a82c9ac3ec876e5d7d1355fd2eb8465f8859
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-04-15 20:00:54 +0530

    Update game.js

--- game.js
+++ game.js
@@ -18,49 +18,56 @@ let questions = [
         choice1: "Head",
         choice2: "Body",
         Choice3: "Style",
-        Choice4: "Folder"
+        Choice4: "Folder",
+        answer: 2
     },
     {
         question: "What is the use of the <noscript> tag in Javascript?",
         choice1: " The contents are displayed by non-JS-based browsers.",
         choice2: " Clears all the cookies",
         choice3: " Both A and B",
-        choice4: " None of the above"
+        choice4: " None of the above",
+        answer: 1
     },
     {
         question: "The correct sequence of HTML tags for starting a webpage is -",
         choice1: "Head, Title, HTML, body",
         choice2: "HTML, Body, Title, Head",
         choice3: "HTML, Head, Title, Body",
-        choice4: "HTML, Head, Title, Body"
+        choice4: "HTML, Title, Head, Body",
+        answer: 3
     },
     {
         question: " Which of the following is not javascript data types?",
         choice1: " Null type",
         choice2: " Undefined type",
         choice3: " Number type",
-        choice4: " All of the mentioned"
+        choice4: " None of the above",
+        answer: 4
     },
     {
         question: " <input> is -",
         choice1: "a format tag",
         choice2: "an empty tag",
         choice3: "All of the above",
-        choice4: "None of the above"
+        choice4: "None of the above", 
+        answer: 2
     },
     {
         question: "Which of the following HTML tag is used to display the text with scrolling effect?",
         choice1: "<marquee>",
         choice2: "<scroll>",
         choice3: "<div>",
-        choice4: "None of the above"
+        choice4: "None of the above",
+        answer: 1
     },
     {
         question: "Which of the following can be used to call a JavaScript Code Snippet?",
         choice1: "Function/Method",
         choice2: "Preprocessor",
         choice3: "Triggering Event",
-        choice4: "RMI  "
+        choice4: "RMI",
+        answer: 1
     },
     {
         question: "",
@@ -100,3 +107,26 @@ let questions = [
     
 
 ]
+
+//constants
+const CORRECT_BONUS=10;
+const MAX_QUESTIONS = 3;
+
+startGame =()=>{
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestio = () => {
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    CurrentQuestion = availableQuestions[questionIndex];
+    question.innerText
+
+}
+
+startGame();
